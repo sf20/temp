3d44160d8d8668ee79089c0475b6809ff51f4ecd
yanl <yanl@yxt.com>
2017-07-19 19:33:10 +0800

    重写hashcode和equals方法

--- openDemo/src/openDemo/entity/OuInfoEntity.java
+++ openDemo/src/openDemo/entity/OuInfoEntity.java
@@ -101,4 +101,29 @@ public class OuInfoEntity {
 		this.orderIndex = orderIndex;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((ID == null) ? 0 : ID.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OuInfoEntity other = (OuInfoEntity) obj;
+		if (ID == null) {
+			if (other.ID != null)
+				return false;
+		} else if (!ID.equals(other.ID))
+			return false;
+		return true;
+	}
+
 }
--- openDemo/src/openDemo/entity/UserInfoEntity.java
+++ openDemo/src/openDemo/entity/UserInfoEntity.java
@@ -282,4 +282,29 @@ public class UserInfoEntity {
 		this.spare10 = spare10;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((ID == null) ? 0 : ID.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UserInfoEntity other = (UserInfoEntity) obj;
+		if (ID == null) {
+			if (other.ID != null)
+				return false;
+		} else if (!ID.equals(other.ID))
+			return false;
+		return true;
+	}
+
 }
