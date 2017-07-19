d6f7154b92255e9959e8845f31bf5ec95f954b0c
yanl <yanl@yxt.com>
2017-07-19 16:04:20 +0800

    完成用户全量和增量同步

--- openDemo/src/openDemo/service/sync/OpSyncService.java
+++ openDemo/src/openDemo/service/sync/OpSyncService.java
@@ -5,8 +5,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
-import java.lang.reflect.InvocationTargetException;
 import java.nio.charset.Charset;
+import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -32,6 +32,7 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import openDemo.dao.UserInfoDao;
 import openDemo.entity.OuInfoEntity;
 import openDemo.entity.ResultEntity;
 import openDemo.entity.UserInfoEntity;
@@ -64,9 +65,20 @@ public class OpSyncService {
 	private static final String ORG_RES_DATA_KEY = "SapMiddleOrg";
 	private static final String EMP_RES_DATA_KEY = "SapMiddleEmp";
 
+	private static final String SERVICEOPERATION_EMP = "QueryEmpInfo";
+	private static final String SERVICEOPERATION_ORG = "QueryOrgInfo";
+	private static final String MODE_FULL = "1";
+	private static final String MODE_UPDATE = "2";
+
 	private static String REPLACE_FROM = "&";
 	private static String REPLACE_TO = " ";
 	private static int SIZE_PER_SYNC = 1500;// 每次同步的数量 当每次同步数量过大(同步时间超过5分钟)会导致网关超时504错误
+
+	private static String MAPKEY_USER_SYNC_ADD = "userSyncAdd";
+	private static String MAPKEY_USER_SYNC_UPDATE = "userSyncUpdate";
+	private static String MAPKEY_USER_ENABLE = "userEnable";
+	private static String MAPKEY_USER_DISABLE = "userDisable";
+	// private static String MAPKEY_USER_DELETE = "userDelete";
 
 	private static SimpleDateFormat javaDateFormat = new SimpleDateFormat("yyyy-MM-dd");
 	private static SimpleDateFormat jsonDateFormat = new SimpleDateFormat("yyyyMMdd");
@@ -74,6 +86,7 @@ public class OpSyncService {
 	private OrgService orgService = new OrgService();
 	private UserService userService = new UserService();
 	private ObjectMapper mapper;
+	private UserInfoDao userInfoDao;
 
 	public OpSyncService() {
 		// 创建用于json反序列化的对象
@@ -82,6 +95,42 @@ public class OpSyncService {
 		mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
 		// json字符串的日期格式
 		mapper.setDateFormat(jsonDateFormat);
+
+		userInfoDao = new UserInfoDao();
+	}
+
+	/**
+	 * 对外提供的同步方法
+	 * 
+	 * @throws IOException
+	 * @throws ReflectiveOperationException
+	 * @throws SQLException
+	 */
+	public void sync() throws IOException, ReflectiveOperationException, SQLException {
+		int count = 0;
+		count = userInfoDao.getAllCount();
+
+		if (count > 0) {
+			// 已经同步过 进行增量同步
+			// 二次同步==>数据比较==>数据库数据更新
+
+			// 用户增量同步
+			opUserSync(SERVICEOPERATION_EMP, MODE_UPDATE);
+		} else {
+			// 组织初次同步<==数据库数据新增
+			// 用户初次同步<==数据库数据新增
+			// 初次同步 进行全量同步
+
+			// 组织全量
+			opOrgSync(SERVICEOPERATION_ORG, MODE_FULL);
+			// 用户全量
+			opUserSync(SERVICEOPERATION_EMP, MODE_FULL);
+			// 岗位全量
+
+		}
+
+		// 同步异常记录
+
 	}
 
 	/**
@@ -93,7 +142,7 @@ public class OpSyncService {
 	 *            可在1（全量）和2（增量）中二选一。EMP拥有1和2两种模式。Org只有1，全量模式。
 	 * @return 响应的json字符串
 	 */
-	public String getJsonPost(String serviceOperation, String mode) {
+	private String getJsonPost(String serviceOperation, String mode) {
 		HttpClient httpClient = HttpClientBuilder.create().build();
 
 		HttpPost httpPost = new HttpPost(REQUEST_URL);
@@ -192,29 +241,21 @@ public class OpSyncService {
 
 	/**
 	 * 组织同步 用时：80-90s
+	 * 
+	 * @param serviceOperation
+	 * @param mode
+	 * @throws IOException
+	 * @throws ReflectiveOperationException
 	 */
-	public void opOrgSync() {
-		// String jsonString = service.getJsonPost("QueryOrgInfo", "1");
-		// System.out.println(jsonString);
-
-		String jsonString = getFileStr("/new_depts.txt");
+	public void opOrgSync(String serviceOperation, String mode) throws IOException, ReflectiveOperationException {
+		String jsonString = getJsonPost(serviceOperation, mode);
 
 		OpReqJsonModle<OpOuInfoModel> modle = new OpReqJsonModle<>();
-		try {
-			modle = mapper.readValue(jsonString, new TypeReference<OpReqJsonModle<OpOuInfoModel>>() {
-			});
-		} catch (IOException e1) {
-			// TODO
-			e1.printStackTrace();
-		}
+		modle = mapper.readValue(jsonString, new TypeReference<OpReqJsonModle<OpOuInfoModel>>() {
+		});
 
 		List<OuInfoEntity> list = null;
-		try {
-			list = copyCreateEntityList(modle.getEsbResData().get(ORG_RES_DATA_KEY), OuInfoEntity.class);
-		} catch (IllegalAccessException | InvocationTargetException | InstantiationException e) {
-			// TODO
-			e.printStackTrace();
-		}
+		list = copyCreateEntityList(modle.getEsbResData().get(ORG_RES_DATA_KEY), OuInfoEntity.class);
 
 		replaceIllegalChar(list);
 
@@ -232,12 +273,10 @@ public class OpSyncService {
 	 * @param toListClassType
 	 *            复制目标对象的类型
 	 * @return 复制后的对象集合
-	 * @throws IllegalAccessException
-	 * @throws InvocationTargetException
-	 * @throws InstantiationException
+	 * @throws ReflectiveOperationException
 	 */
 	private <E, T> List<T> copyCreateEntityList(List<E> fromList, Class<T> toListClassType)
-			throws IllegalAccessException, InvocationTargetException, InstantiationException {
+			throws ReflectiveOperationException {
 		List<T> entityList = null;
 
 		int listSize = fromList.size();
@@ -256,32 +295,241 @@ public class OpSyncService {
 
 	/**
 	 * 用户同步
+	 * 
+	 * @param serviceOperation
+	 * @param mode
+	 * @throws IOException
+	 * @throws ReflectiveOperationException
+	 * @throws SQLException
 	 */
-	public void opUserSync() {
-		// String jsonString = service.getJsonPost("QueryEmpInfo", "2");
-		// System.out.println(jsonString);
-		String jsonString = getFileStr("/userM2.json");
+	public void opUserSync(String serviceOperation, String mode)
+			throws IOException, ReflectiveOperationException, SQLException {
+		String jsonString = getJsonPost(serviceOperation, mode);
 
 		OpReqJsonModle<OpUserInfoModel> modle = new OpReqJsonModle<>();
-		try {
-			modle = mapper.readValue(jsonString, new TypeReference<OpReqJsonModle<OpUserInfoModel>>() {
-			});
-		} catch (IOException e) {
-			// TODO
-			e.printStackTrace();
-		}
+		modle = mapper.readValue(jsonString, new TypeReference<OpReqJsonModle<OpUserInfoModel>>() {
+		});
 
-		List<UserInfoEntity> list = null;
-		try {
-			list = copyCreateEntityList(modle.getEsbResData().get(EMP_RES_DATA_KEY), UserInfoEntity.class);
-		} catch (IllegalAccessException | InvocationTargetException | InstantiationException e) {
-			// TODO
-			e.printStackTrace();
+		List<UserInfoEntity> newList = null;
+		newList = copyCreateEntityList(modle.getEsbResData().get(EMP_RES_DATA_KEY), UserInfoEntity.class);
+
+		// 全量模式
+		if (MODE_FULL.equals(mode)) {
+			if (newList.size() > 0) {
+				syncAddOneByOne(newList);
+			}
+		}
+		// 增量模式
+		else {
+			// 获取数据库全量list
+			List<UserInfoEntity> allList = userInfoDao.getAll();
+			// 与增量list进行比较
+			Map<String, List<UserInfoEntity>> map = compareList(allList, newList);
+
+			List<UserInfoEntity> usersToSyncAdd = map.get(MAPKEY_USER_SYNC_ADD);
+			if (usersToSyncAdd.size() > 0) {
+				syncAddOneByOne(usersToSyncAdd);
+			}
+
+			List<UserInfoEntity> usersToSyncUpdate = map.get(MAPKEY_USER_SYNC_UPDATE);
+			if (usersToSyncUpdate.size() > 0) {
+				syncUpdateOneByOne(usersToSyncUpdate);
+			}
+
+			List<UserInfoEntity> usersToDisable = map.get(MAPKEY_USER_DISABLE);
+			if (usersToDisable.size() > 0) {
+				syncDisableOneByOne(usersToDisable);
+			}
+
+			List<UserInfoEntity> usersToEnable = map.get(MAPKEY_USER_ENABLE);
+			if (usersToEnable.size() > 0) {
+				syncEnableOneByOne(usersToEnable);
+			}
 		}
 
 		// TODO to delete
-		printUserInfo(list);
+		// printUserInfo(list);
 
+	}
+
+	/**
+	 * 逐个用户同步新增
+	 * 
+	 * @param usersToSyncAdd
+	 * @throws SQLException
+	 */
+	private void syncAddOneByOne(List<UserInfoEntity> usersToSyncAdd) throws SQLException {
+		List<UserInfoEntity> tempList = new ArrayList<>();
+		ResultEntity resultEntity = null;
+		for (UserInfoEntity user : usersToSyncAdd) {
+			tempList.add(user);
+
+			resultEntity = userService.userSync(true, tempList);
+			if ("0".equals(resultEntity.getCode())) {
+				userInfoDao.insert(user);
+			} else {
+				// TODO log
+				print("用户同步新增", resultEntity);
+			}
+
+			tempList.clear();
+		}
+	}
+
+	/**
+	 * 逐个用户同步更新
+	 * 
+	 * @param usersToSyncUpdate
+	 * @throws SQLException
+	 */
+	private void syncUpdateOneByOne(List<UserInfoEntity> usersToSyncUpdate) throws SQLException {
+		List<UserInfoEntity> tempList = new ArrayList<>();
+		ResultEntity resultEntity = null;
+
+		for (UserInfoEntity user : usersToSyncUpdate) {
+			tempList.add(user);
+
+			resultEntity = userService.userSync(true, tempList);
+			if ("0".equals(resultEntity.getCode())) {
+				userInfoDao.update(user);
+			} else {
+				// TODO log
+				print("用户同步更新", resultEntity);
+			}
+
+			tempList.clear();
+		}
+
+	}
+
+	/**
+	 * 逐个用户同步启用
+	 * 
+	 * @param usersToEnable
+	 * @throws SQLException
+	 */
+	private void syncEnableOneByOne(List<UserInfoEntity> usersToEnable) throws SQLException {
+		List<String> tempList = new ArrayList<>();
+		ResultEntity resultEntity = null;
+
+		for (UserInfoEntity user : usersToEnable) {
+			tempList.add(user.getUserName());
+
+			resultEntity = userService.enabledusersSync(tempList);
+			if ("0".equals(resultEntity.getCode())) {
+				userInfoDao.update(user);
+			} else {
+				// TODO log
+				print("用户同步启用", resultEntity);
+			}
+
+			tempList.clear();
+		}
+	}
+
+	/**
+	 * 逐个用户同步禁用
+	 * 
+	 * @param usersToDisable
+	 * @throws SQLException
+	 */
+	private void syncDisableOneByOne(List<UserInfoEntity> usersToDisable) throws SQLException {
+		List<String> tempList = new ArrayList<>();
+		ResultEntity resultEntity = null;
+		for (UserInfoEntity user : usersToDisable) {
+			tempList.add(user.getUserName());
+
+			resultEntity = userService.disabledusersSync(tempList);
+			if ("0".equals(resultEntity.getCode())) {
+				userInfoDao.update(user);
+			} else {
+				// TODO log
+				print("用户同步禁用", resultEntity);
+			}
+
+			tempList.clear();
+		}
+	}
+
+	/**
+	 * 数据库表数据集合与最新获取数据集合进行比较
+	 * 
+	 * @param fullList
+	 *            数据库表数据集合
+	 * @param newList
+	 *            最新获取数据集合
+	 * @return 包含 同步新增、更新、启用、禁用等用户集合的Map对象
+	 */
+	private Map<String, List<UserInfoEntity>> compareList(List<UserInfoEntity> fullList, List<UserInfoEntity> newList) {
+		Map<String, List<UserInfoEntity>> map = new HashMap<>();
+
+		List<UserInfoEntity> usersToSyncAdd = new ArrayList<>();
+		List<UserInfoEntity> usersToSyncUpdate = new ArrayList<>();
+		List<UserInfoEntity> usersToEnable = new ArrayList<>();
+		List<UserInfoEntity> usersToDisable = new ArrayList<>();
+
+		// 待更新用户
+		for (UserInfoEntity fullUser : fullList) {
+			for (UserInfoEntity newUser : newList) {
+				// 已经存在的用户比较
+				if (fullUser.equals(newUser)) {
+					if (fullUser.getExpireDate() == null) {
+						if (newUser.getExpireDate() != null) {
+							// 用户过期禁用
+							usersToDisable.add(newUser);
+						} else {
+							// 存在用户更新
+							usersToSyncUpdate.add(newUser);
+						}
+					} else {
+						if (newUser.getExpireDate() == null) {
+							// 用户重新启用
+							usersToEnable.add(newUser);
+						} else {
+							// 存在用户更新
+							usersToSyncUpdate.add(newUser);
+						}
+					}
+				}
+			}
+		}
+
+		// 待新增用户
+		for (UserInfoEntity user : newList) {
+			if (!fullList.contains(user)) {
+				usersToSyncAdd.add(user);
+			}
+		}
+
+		// TODO 待删除用户
+
+		map.put(MAPKEY_USER_SYNC_ADD, usersToSyncAdd);
+		map.put(MAPKEY_USER_SYNC_UPDATE, usersToSyncUpdate);
+		map.put(MAPKEY_USER_ENABLE, usersToEnable);
+		map.put(MAPKEY_USER_DISABLE, usersToDisable);
+
+		return map;
+	}
+
+	/**
+	 * 获取用户名集合
+	 * 
+	 * @param list
+	 * @return
+	 */
+	private List<String> getUserNameList(List<UserInfoEntity> list) {
+		List<String> nameList = new ArrayList<>();
+		for (UserInfoEntity user : list) {
+			nameList.add(user.getUserName());
+		}
+		return nameList;
+	}
+
+	/**
+	 * 分批同步
+	 */
+	private void temp() {
+		List<UserInfoEntity> list = null;
 		ResultEntity resultEntity = new ResultEntity();
 		int listSize = list.size();
 		int syncTimes = calcSyncTimes(listSize, SIZE_PER_SYNC);
@@ -294,8 +542,33 @@ public class OpSyncService {
 						list.subList(count * SIZE_PER_SYNC, (count + 1) * SIZE_PER_SYNC));
 			}
 
-			// TODO to delete
 			print("用户同步" + (count + 1), resultEntity);
+		}
+
+		// =========
+		// 添加数据到数据库
+		List<UserInfoEntity> newList = new ArrayList<>();
+		boolean isInsertFail = false;
+		try {
+			userInfoDao.insertBatch(newList);
+		} catch (SQLException e) {
+			isInsertFail = true;
+			e.printStackTrace();
+		}
+
+		if (!isInsertFail) {
+			// 同步数据
+			List<UserInfoEntity> tempList = new ArrayList<>();
+			ResultEntity resultEntity2 = new ResultEntity();
+			int count = 0;
+			for (UserInfoEntity user : newList) {
+				tempList.clear();
+				tempList.add(user);
+				resultEntity2 = userService.userSync(false, tempList);
+				print("用户同步" + (++count), resultEntity2);
+			}
+		} else {
+			System.out.println("出错了，未同步。。。");
 		}
 	}
 
@@ -330,7 +603,7 @@ public class OpSyncService {
 		}
 	}
 
-	// TODO====================测试用方法======================
+	// TODO to delete====================测试用方法======================
 	private void printOrgInfo(List<OuInfoEntity> list) {
 		System.out.println("ID == OuName == ParentID == Description == Users == isSub");
 		for (OuInfoEntity org : list) {
