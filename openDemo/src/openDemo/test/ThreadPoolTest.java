d53534e785ee268e19a57b2c23618d483cb1b86f
yanl <yanl@yxt.com>
2017-07-27 13:21:34 +0800

    定时器模拟测试逻辑优化

--- openDemo/src/openDemo/test/ThreadPoolTest.java
+++ openDemo/src/openDemo/test/ThreadPoolTest.java
@@ -13,16 +13,16 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class ThreadPoolTest {
-	private static final long PERIOD = 60 * 1000;// 定时器间隔执行时间 单位毫秒
+	private static final long PERIOD = 10 * 60 * 1000;// 定时器间隔执行时间 单位毫秒
 
 	// 每次定时器执行时间参数
-	private static final int TIMER_EXEC_TIME_HOUR = 0;
-	private static final int TIMER_EXEC_TIME_MINUTE = 15;
+	private static final int TIMER_EXEC_TIME_HOUR = 11;
+	private static final int TIMER_EXEC_TIME_MINUTE = 20;
 	private static final int TIMER_EXEC_TIME_SECOND = 00;
 
 	private static final int CORE_POOL_SIZE = 1;// 线程池数量
 
-	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 	private static final Logger logger = LogManager.getLogger();
 
 	private Calendar calendar;
@@ -43,7 +43,7 @@ public class ThreadPoolTest {
 
 	public static void main(String[] args) {
 
-		logger.info("定时器开始时间：" + dateFormat.format(new Date()));
+		logger.info("定时器已启动...");
 		new ThreadPoolTest().multiSyncTask();
 
 		// for (int i = 0; i < 10; i++) {
@@ -62,7 +62,7 @@ public class ThreadPoolTest {
 					logger.info("开始执行task：" + dateFormat.format(new Date()));
 					// 预定下次执行时间
 					Date nextTime = getNextTime();
-					logger.info("定时器预定下次执行时间: " + dateFormat.format(nextTime));
+					// logger.info("定时器预定下次执行时间: " + dateFormat.format(nextTime));
 
 					// 模拟任务执行耗时
 					long randomTime = randomTime();
@@ -75,8 +75,6 @@ public class ThreadPoolTest {
 
 					// 预定与实际进行比较
 					long delay = compareGetDelay(taskEndTime, nextTime);
-					logger.info("修改后预定下次执行时间: " + dateFormat.format(addTime(taskEndTime, delay)));
-					logger.info("距离下次执行时间还有: " + delay / (1000 * 60) + "分" + delay / 1000 + "秒");
 					logger.info(System.lineSeparator());
 
 					threadPool.schedule(this, delay, TimeUnit.MILLISECONDS);
@@ -104,25 +102,31 @@ public class ThreadPoolTest {
 	 * @return
 	 */
 	public long compareGetDelay(Date nowTime, Date execTimeExpected) {
-		if (nowTime.compareTo(execTimeExpected) <= 0) {
-			return execTimeExpected.getTime() - nowTime.getTime();
-		} else {
+		long delay = 0;
 
-			long delay = 0;
+		if (nowTime.compareTo(execTimeExpected) <= 0) {
+			delay = execTimeExpected.getTime() - nowTime.getTime();
+		} else {
+			// 超时时间差
 			long timeGap = nowTime.getTime() - execTimeExpected.getTime();
-			// 整除的处理
-			if (timeGap % PERIOD > 0) {
-				delay = (timeGap / PERIOD + 1) * PERIOD;
-			} else {
-				delay = timeGap / PERIOD * PERIOD;
-			}
-			return addTime(execTimeExpected, delay).getTime() - nowTime.getTime();
+			// 超时周期的处理
+			delay = addTime(execTimeExpected, (timeGap / PERIOD + 1) * PERIOD).getTime() - nowTime.getTime();
 		}
 
+		logger.info("预定下次执行时间: " + dateFormat.format(addTime(nowTime, delay)));
+		logger.info("距离下次执行还有: " + delay / (1000 * 60) + "分" + (delay % (1000 * 60)) / 1000 + "秒");
+
+		return delay;
 	}
 
+	/**
+	 * 返回定时器每次预定执行时间
+	 * 
+	 * @return
+	 */
 	public Date getNextTime() {
 		Date nextTime = addTime(baseDate, PERIOD);
+		// 调整下次计算基准日
 		baseDate = nextTime;
 
 		return nextTime;
@@ -145,7 +149,7 @@ public class ThreadPoolTest {
 	}
 
 	public static long randomTime() {
-		return (new Random().nextInt(60) + 20) * 1000;
+		return (new Random().nextInt(1200) + 240) * 1000;
 	}
 
 }
