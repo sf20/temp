c19a577329f7768a1c4749ac146657765b2d9977
yanl <yanl@yxt.com>
2017-08-31 17:57:38 +0800

    配置修改

--- openDemo/src/openDemo/config/LeoConfig.java
+++ openDemo/src/openDemo/config/LeoConfig.java
@@ -2,8 +2,8 @@ package openDemo.config;
 
 public interface LeoConfig {
 	String apikey = "abb15aa6-3b5d-4160-97f0-12366c0cfd06";
-	
-	String secretkey  = "cc734695-aa30-413a-b59a-7326403694a3";
-	
-	String baseUrl = "http://leogroup.qida.yunxuetang.com.cn/";
+
+	String secretkey = "cc734695-aa30-413a-b59a-7326403694a3";
+
+	String baseUrl = "http://api.qida.yunxuetang.com.cn/";
 }
